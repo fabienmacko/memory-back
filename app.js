f1c2f4349be6b5110676b266e886a9609b37b901
Fabien Mackowiak <fmackodev@gmail.com>
2020-04-01 23:05:40 +0200

    adding image sending from server and user turn management

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 
 const app = express();
+
 app.use(index);
+
+app.use('/images', express.static('images'));
 
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Listening on port ${port}`));
@@ -14,29 +17,62 @@ const io = socketIo(server);
 
 let usersSessions = [];
 
-const startGame = socket => {
+
+const shuffle = array => {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+const startGame = io => {
+  // Randomly choose the first player
+  console.log(usersSessions);
   
+  io.sockets.emit('player', usersSessions[Math.floor(Math.random() * 2)]);
+  io.sockets.emit('images', shuffle([1,2,3,4,5,6,1,2,3,4,5,6]));
 }
 
 io.on("connection", socket => {
 
-  socket.on('pseudo', pseudo => {
-    usersSessions.push({
-      pseudo,
-      id: socket.id
+  // Send pseudo of the user to the front
+  const waitPseudo = () => new Promise(resolve => {
+    socket.on('pseudo', pseudo => {
+      usersSessions.push({
+        pseudo,
+        id: socket.id
+      });
+      resolve();
     });
   });
 
+  // Start a game
+  waitPseudo().then(() => {
+    if (usersSessions.length === 2) {
+      io.sockets.emit('startGame', 'The game is starting !');
   
-  if (usersSessions.length === 1) {
-    io.sockets.emit('startGame', 'The game is starting !');
+      startGame(io);
+    }
+  })
 
-    startGame(socket);
-  }
   
   socket.on("disconnect", () => {
     console.log("Client disconnected");
     // Remove user from active sessions
     usersSessions.splice(usersSessions.indexOf(usersSessions.find(session => session.id === socket.id)), 1);
   });
+
 });
